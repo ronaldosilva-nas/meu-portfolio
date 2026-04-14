49fe1a14f0bb50a3e90997822afdb33ae88923f5
Ronaldo Silva Nascimento <silvaronaldo7662@gmail.com>
2026-04-14 13:51:41 -0300

    implementação do menu do topo

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { HeaderService } from '../services/header';
+import { HeaderModel } from '../models/header';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
 
   constructor(private headerService: HeaderService) {}
+
+  menuStatus = 'closed';
+
+  menuItems?: Array<HeaderModel>;
   
+  ngOnInit() {
+    const menu = this.headerService.getItems();
+
+    this.menuItems = menu;
+  }
+
+  menuClick() {
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open');
+  }
 }
